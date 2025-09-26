22a1ebe36f8eb97b5741632467ce471e35c3f7a8
Noubata Benoit <benynouba@gmail.com>
2025-09-26 12:03:43 +0100

    first commit

---
+++ Gate One/TaskEight.java
@@ -0,0 +1,38 @@
+public class TaskEight{
+public static void main(String[]args){
+
+	int number = 0;
+	int number1 = 0;
+	int number2 = 0;
+	int number3 = 0;
+	int number4 = 0;
+	int sum = 0;
+	int sum1 = 0;
+
+	int divide1 = 0;
+
+	while(number < 10){
+	number++;
+
+	if (number % 4 == 0){
+	
+	
+	number1 = number*number;
+	number2 = number1*number;
+	number3 = number2*number;
+	number4 = number3*number;
+	
+	sum = number+number1+number2+number3+number4;
+	
+	divide1 = sum/10;
+	divide2 = sum%10;
+	System.out.printf("%d %d", divide1, divide2);
+	
+	}
+	
+	
+	}
+
+	
+}
+}
