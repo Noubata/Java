fc1c019ba226371f9abafa402abace757bc7e9ca
Noubata Benoit <benynouba@gmail.com>
2025-10-04 23:31:25 +0100

    functions

--- Kata/KataTest1.java
+++
@@ -1,9 +0,0 @@
-public class KataTest1{
-
-	public static void main(String[]args){
-	
-	System.out.println(Kata1.isLeapYear(17));
-	System.out.println(Kata1.isLeapYear(1800));
-	Kata1.printStars(6);
-	}
-}
